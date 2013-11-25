059ef651e54dafd7ab4a001884f42595a4efbc88
Ravi Shankar <nagarro@Nagarros-MacBook-Pro-5.local>
2013-11-25 17:05:18 +0530

    Initial Commit

---
+++ UIDynamicsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIDynamicsDemo
+//
+//  Created by Ravi Shankar on 11/25/13.
+//  Copyright (c) 2013 Nagarro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIDynamicsDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIDynamicsDemo
+//
+//  Created by Ravi Shankar on 11/25/13.
+//  Copyright (c) 2013 Nagarro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
